c7b6fee336129ec03e8c8881d6b3a7afe41ba323
Joshua Roesslein <jroesslein@gmail.com>
2010-01-15 17:07:14 -0600

    Update harvester script.

--- streaming/harvester.py
+++ streaming/harvester.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 Twitter Streaming API Harvester
 
@@ -10,27 +11,46 @@ Requires: Tweepy
 """
 
 from optparse import OptionParser
+from sys import exit
 from tweepy import Stream, StreamListener
 
 
 class HarvesterListener(StreamListener):
 
+    def __init__(self, dest_file):
+        StreamListener.__init__(self)
+        self.file = open(dest_file, 'ab')
+
     def on_data(self, data):
-        # TODO: write data to file
-        return
+        self.file.write(data)
 
 
 def main(options, args):
 
-    print 'Nothing to see here yet!'
+    listener = HarvesterListener(options.file)
 
-    
+    try:
+        username, password = args[0], args[1]
+    except IndexError:
+        print 'Missing username/password!'
+        exit(1)
+
+    stream = Stream(username, password, listener)
+
+    try:
+        stream.sample()
+    except KeyboardInterrupt:
+        exit(0)
+
+ 
 if __name__ == '__main__':
 
     opt = OptionParser(
-        description='Twitter Streaming API harvester'
+        description='Twitter Streaming API harvester',
+		usage='harvester [options] <username> <password>'
     )
-    opt.add_option('-i', '--interval', type='int', dest='interval', default=3600, help='Interval in seconds to split files')
+    opt.add_option('-f', '--file', dest='file', default='output.json',
+		help='file to write statuses into')
 
     options, args = opt.parse_args()
     main(options, args)
